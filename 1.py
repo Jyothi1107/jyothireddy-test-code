6ca81f05c140af1b050ae1b1bb2a852faa9b52f1
Jyothi1107 <118727842+Jyothi1107@users.noreply.github.com>
2022-11-21 16:51:34 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,13 @@
-def numberToBase(n, b):
-    if n == 0:
-        return [0]
-    digits = []
-    while n:
-        digits.append(int(n % b))
-        n //= b
-    return digits[::-1]
+class Solution:
+   def solve(self, n):
+      sign = '-' if n<0 else ''
+      n = abs(n)
+      if n < 3:
+         return str(n)
+         s = ''
+      while n != 0:
+         s = str(n%3) + s
+         n = n//3
+      return sign+s
+ob = Solution()
+print(ob.solve(10))
